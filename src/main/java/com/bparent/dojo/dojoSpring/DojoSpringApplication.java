fba4dbd77b6e36b55bc42017f10007bf36c66ba4
Basile Parent <bparent.ext@orange.com>
2018-07-05 17:03:06 +0200

    Empty Spring Boot

---
+++ src/main/java/com/bparent/dojo/dojoSpring/DojoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.bparent.dojo.dojoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoSpringApplication.class, args);
+	}
+}
