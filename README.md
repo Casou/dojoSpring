18f9e85fc369c64a03fbc5ea4b21ca7242feed91
Basile Parent <bparent.ext@orange.com>
2018-07-05 17:57:04 +0200

    Init DB + Readme

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# dojoSpring
+# Dojo Spring
+
+
+##Step 1
+Renvoyer la liste de tous les `Todo`
+
+* Méthode : _GET_
+* URL : _/todos_
+* Paramètres : _Aucun_
+
+_**Retour attendu** : Une liste de `Todo` avec tous leurs champs._ 
+
+
+##Step x
+Renvoyer la liste de tous les `User` avec leurs `Todo`
+
+* Méthode : _GET_
+* URL : _/users_
+* Paramètres : _Aucun_
+
+_**Retour attendu** : Une liste de `User` avec pour chacun, une liste de `Todo` avec tous leurs champs._
+
+##Step x
+Renvoyer la liste de tous les `User` sans leurs `Todo`
+
+* Méthode : _GET_
+* URL : _/users/clean_
+* Paramètres : _Aucun_
+
+_**Retour attendu** : Une liste de `User` avec tous leurs champs sauf les `Todo`_
+
+##Step x
+Changer d'état un `Todo` (complete / uncomplete)
+
+* Méthode : _PUT_
+* URL : _/todo/complete_
+* Paramètres : JSON
+    ```
+    {
+        id : number,
+        complete : boolean
+    }
+    ```
+_**Retour attendu** : Statut OK (200)_
+
+##Step x.1
+Affecter un `Todo` à un `User` (par défaut uncomplete)
+
+* Méthode : _POST_
+* URL : _/users/todo_
+* Paramètres : JSON
+    ```
+    {
+        user : {
+            id : number
+        },
+        todo : {
+            text : string
+        }
+    }
+    ```
+_**Retour attendu** : Statut OK (200)_
+    
+##Step x.2
+Avant d'affecter un `Todo` à un `User`, on vérifie que le texte ne fasse pas plus de 255 caractères
+
+_**Retour attendu** : Statut Bad Request (400)_
+
+##Step x.3
+Avant d'affecter un `Todo` à un `User`, on vérifie que le texte ne contienne pas plus de 2 fois la lettre N et quatre fois la lettre P.
+
+_**Retour attendu** : Statut Bad Request (400)_
+
+
---
+++ src/test/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO USERS(login, name, first_name, job) VALUES ('gpereira', 'Guillaume', 'Pereira', 'Scrum Master');
+INSERT INTO USERS(login, name, first_name, job) VALUES ('gvaneecloo', 'Guillaume', 'Vaneecloo', 'Architecte');
+INSERT INTO USERS(login, name, first_name, job) VALUES ('mbah', 'Bah', 'Madjou', 'Le nouveau');
+INSERT INTO USERS(login, name, first_name, job) VALUES ('jbricout', 'Jérémy', 'Bricout', 'Glandeur');
+
+INSERT INTO TODO (text, id_user) SELECT 'Préparer la prochaine rétro', id FROM USERS WHERE login = 'gpereira';
+INSERT INTO TODO (text, id_user) SELECT 'Préparer des ice breakers', id FROM USERS WHERE login = 'gpereira';
+INSERT INTO TODO (text, id_user) SELECT 'Aller au sport', id FROM USERS WHERE login = 'gpereira';
+INSERT INTO TODO (text, id_user) SELECT 'Aller chercher les enfants', id FROM USERS WHERE login = 'gpereira';
+INSERT INTO TODO (text, id_user) SELECT 'Mettre en place Rancher', id FROM USERS WHERE login = 'gvaneecloo';
+INSERT INTO TODO (text, id_user) SELECT 'Préparer la transition Spring', id FROM USERS WHERE login = 'gvaneecloo';
+INSERT INTO TODO (text, id_user) SELECT 'Monter en compétence sur ADV', id FROM USERS WHERE login = 'mbah';
+INSERT INTO TODO (text, id_user) SELECT 'Formation sécurité', id FROM USERS WHERE login = 'mbah';
+INSERT INTO TODO (text, id_user) SELECT 'Baby', id FROM USERS WHERE login = 'jbricout';
+INSERT INTO TODO (text, id_user) SELECT 'Sieste', id FROM USERS WHERE login = 'jbricout';
+INSERT INTO TODO (text, id_user) SELECT 'Baby', id FROM USERS WHERE login = 'jbricout';
+INSERT INTO TODO (text, id_user) SELECT 'Café', id FROM USERS WHERE login = 'jbricout';
+INSERT INTO TODO (text, id_user) SELECT 'Baby', id FROM USERS WHERE login = 'jbricout';
